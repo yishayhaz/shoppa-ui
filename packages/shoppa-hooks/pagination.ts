bbaed222da809b060e20f1c0fa58a4e4365767a1
Yishay Hazan <yishay11013@gmail.com>
2023-06-08 15:53:36 +0300

    need pagination better typing

---
+++ packages/shoppa-hooks/pagination.ts
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { UseApiFire, UseApiProps, UseApiResult, useApi } from "./api";
+
+export type UsePaginationProps<T, K extends any[]> = [
+  UseApiProps<T, K>,
+  number,
+  number
+];
+
+export type UsePaginationResult<T, K extends any[]> = UseApiResult<T, K> &
+  UsePaginationFuncProps & {
+    setPage: (page: number, ...args: K) => void;
+    setCount: (count: number, ...args: K) => void;
+  };
+
+export type UsePaginationFuncProps = {
+  page: number;
+  count: number;
+};
+
+export type UsePaginationFunction<T, K extends any[]> = (
+  arg0: UsePaginationFuncProps,
+  ...args: K
+) => Promise<T>;
+
+export const usePagination = <T, K extends any[]>(
+  func: UsePaginationFunction<T, K>
+): UsePaginationResult<T, K> => {
+  const [page, _setPage] = useState<number>(1);
+  const [count, _setCount] = useState<number>(10);
+
+  const api = useApi(func);
+
+  const setPage = (page: number, ...args: K) => {
+    _setPage(page);
+    api.fire(
+      {
+        page,
+        count,
+      },
+      ...args
+    );
+  };
+
+  const setCount = (count: number, ...args: K) => {
+    _setCount(count);
+    api.fire(
+      {
+        page,
+        count,
+      },
+      ...args
+    );
+  };
+
+  return {
+    ...api,
+    fire: api.fire as UseApiFire<any[]>,
+    page,
+    count,
+    setPage,
+    setCount,
+  };
+};
