fc94082db52e56829f37acf0c0ddf9a397f448c1
Yishay Hazan <yishay11013@gmail.com>
2023-06-19 18:05:30 +0300

    better deepClone

--- packages/shoppa-utils/deepClone.ts
+++ packages/shoppa-utils/deepClone.ts
@@ -1,12 +1,20 @@
 import { AnyObject } from "shoppa-ts";
 
-export function deepCopy(obj: AnyObject): AnyObject {
+export function deepClone(obj: AnyObject): AnyObject {
+  if (
+    obj instanceof Function ||
+    obj instanceof RegExp ||
+    obj instanceof Date ||
+    obj === null
+  ) {
+    return obj;
+  }
   if (Array.isArray(obj)) {
-    return obj.map((item) => deepCopy(item));
+    return obj.map((item) => deepClone(item));
   }
   if (Object(obj) === obj) {
     let newObj: AnyObject = {};
-    Object.keys(obj).forEach((key) => (newObj[key] = deepCopy(obj[key])));
+    Object.keys(obj).forEach((key) => (newObj[key] = deepClone(obj[key])));
     return newObj;
   }
   return obj;
