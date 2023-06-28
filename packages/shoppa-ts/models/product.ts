de74f0942eea59391e8c152d04e7b0b7ffe1c3fc
Yishay Hazan <yishay11013@gmail.com>
2023-06-28 17:29:55 +0300

    added price to product preview

--- packages/shoppa-ts/models/product.ts
+++ packages/shoppa-ts/models/product.ts
@@ -22,6 +22,7 @@ export type MProductPopulated = MProduct & {
 
 export type MProductPreview = BaseModel & {
   name: string;
+  price: number;
   store: MProductStore;
   brand: ProductBrand;
   assets: ApiFile[];
