e0c6927eb4de3b183b6df459ffcfb395c4ffaa5d
Yishay Hazan <yishay11013@gmail.com>
2023-07-02 09:39:24 +0300

    useFilters

--- docs/shoppa-ui/src/screens/filters.tsx
+++ docs/shoppa-ui/src/screens/filters.tsx
@@ -1,13 +1,74 @@
 import { useFilters } from "@shoppa-hooks/filters";
-import { useLocation } from "react-router-dom";
+import { Input } from "@shoppa-ui/widgets/input";
+import { Radio } from "@shoppa-ui/widgets/radio";
 
 export function FiltersScreen() {
-  const location = useLocation();
-  const filters = useFilters({ name: "string", age: "number" }, [location]);
+  const filters = useFilters({
+    name: "string",
+    age: "number",
+    from: "date",
+    to: "date",
+    include: "boolean",
+  });
+
+  console.log(filters.asValues);
 
   return (
     <div>
       <h1>This is the documentation for the Filters</h1>
+      <br />
+      <pre>
+        <code>{JSON.stringify(filters.searchParams, null, 2)}</code>
+      </pre>
+      <div className="d-flex gap-6">
+        <Input
+          name="name"
+          value={filters.asValues.name ?? ""}
+          onChange={(e) => filters.onChange("name", e.target.value)}
+        />
+        <Input
+          name="age"
+          value={filters.asValues.age ?? ""}
+          onChange={(e) => filters.onChange("age", e.target.value)}
+        />
+        <Input
+          name="from"
+          value={filters.asValues.from ?? ""}
+          onChange={(e) => filters.onChange("from", e.target.value)}
+          type="date"
+          max={filters.asValues.to ?? ""}
+        />
+        <Input
+          name="to"
+          value={filters.asValues.to ?? ""}
+          onChange={(e) => filters.onChange("to", e.target.value)}
+          min={filters.asValues.from ?? ""}
+          type="date"
+        />
+      </div>
+      <br />
+      <hr />
+      <br />
+      <div>
+        <Radio
+          label="yes"
+          value="true"
+          name="include"
+          onChange={(e) => filters.onChange("include", e.target.value)}
+        />
+        <Radio
+          label="no"
+          value="false"
+          name="include"
+          onChange={(e) => filters.onChange("include", e.target.value)}
+        />
+        <Radio
+          label="ignore"
+          value=""
+          name="include"
+          onChange={(e) => filters.onChange("include", e.target.value)}
+        />
+      </div>
     </div>
   );
 }
--- packages/shoppa-hooks/filters.ts
+++ packages/shoppa-hooks/filters.ts
@@ -1,35 +1,119 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { AnyObject } from "shoppa-ts";
 import { getSearchParams } from "shoppa-utils/params";
 
 export type Filters = { [key: string]: Filter };
 
 export type Filter = "string" | "number" | "boolean" | "date";
 
-export const useFilters = (filters: Filters, dep: any[]) => {
-  const [searchParams, setSearchParams] = useState(
-    {} as { [key: string]: string }
-  );
+export type UseFiltersOnChange = (key: string, value: string) => void;
 
-  const handleFilters = () => {
+export type UseFiltersSearchParam = {
+  [key: string]: UseFiltersSearchParamValue;
+};
+
+export type UseFiltersSearchParamValue = string | number | boolean | Date;
+
+export type UseFiltersReturn = {
+  onChange: UseFiltersOnChange;
+  searchParams: UseFiltersSearchParam;
+  asValues: AnyObject;
+};
+
+export const useFilters = (filters: Filters): UseFiltersReturn => {
+  const [searchParams, setSearchParams] = useState<UseFiltersSearchParam>({});
+
+  const _extractInitialFilters = () => {
     const searchParams = getSearchParams(...Object.keys(filters));
+    const params: AnyObject = {};
 
     for (const [key, value] of Object.entries(searchParams)) {
-      const filter = filters[key];
-      if (filter === "string") {
-        // ...
-      } else if (filter === "number") {
-        // ...
-      } else if (filter === "boolean") {
-        // ...
-      } else if (filter === "date") {
-        // ...
-      }
+      const param = _convertValueByType(filters[key], value);
+
+      if (param === null) continue;
+
+      params[key] = param;
+    }
+
+    setSearchParams(params);
+  };
+
+  const _convertValueByType = (type: string, value: string) => {
+    if (value === "") return null;
+
+    if (type === "string") {
+      return value;
+    }
+
+    if (type === "number") {
+      if (isNaN(Number(value))) return null;
+      return Number(value);
+    }
+
+    if (type === "boolean") {
+      if (!["true", "false", true, false].includes(value)) return null;
+      return value === "true";
+    }
+
+    if (type === "date") {
+      let _value: UseFiltersSearchParamValue = value;
+      if (Number.isInteger(Number(_value))) _value = Number(_value);
+
+      if (new Date(_value).toString() === "Invalid Date") return null;
+      return new Date(_value).getTime();
     }
   };
 
-  useEffect(() => {
-    handleFilters();
-  }, dep);
+  const _goToParams = useCallback(
+    (params = searchParams) => {
+      const newParams: AnyObject = {};
+      for (const [key, value] of Object.entries(params)) {
+        newParams[key] = value.toString();
+      }
+      const searchParams = new URLSearchParams(newParams);
+      window.history.pushState({}, "", `?${searchParams.toString()}`);
+    },
+    [searchParams]
+  );
 
-  return null;
+  useEffect(() => {
+    _extractInitialFilters();
+  }, []);
+
+  useEffect(() => {
+    _goToParams();
+  }, [searchParams]);
+
+  const onChange: UseFiltersOnChange = (key, value) => {
+    if (!filters[key]) return;
+
+    const param = _convertValueByType(filters[key], value);
+    const newSearchParams = { ...searchParams };
+
+    if (param === null || typeof param === "undefined") {
+      delete newSearchParams[key];
+    } else {
+      newSearchParams[key] = param;
+    }
+
+    setSearchParams(newSearchParams);
+  };
+
+  const asValues = useMemo(() => {
+    const values: AnyObject = {};
+    for (const [key, value] of Object.entries(searchParams)) {
+      if (filters[key] === "date" && typeof value !== "boolean") {
+        values[key] = new Date(value).toISOString().split("T")[0];
+      } else {
+        values[key] = value.toString();
+      }
+    }
+    return values;
+  }, [searchParams]);
+
+  return {
+    onChange,
+    searchParams,
+    asValues,
+  };
 };
