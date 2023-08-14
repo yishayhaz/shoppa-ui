cb31dd48d29a16ffa3c1e458ddf7195b98ef9688
Yishay Hazan <yishay11013@gmail.com>
2023-08-14 18:29:36 +0300

    shoppa ts

--- packages/shoppa-ts/models/product.ts
+++ packages/shoppa-ts/models/product.ts
@@ -1,5 +1,6 @@
 import { ApiFile, BaseModel, RefField } from "../common";
 import { MVariant } from "./variant";
+import { MStore } from "./store";
 
 export type MProduct = BaseModel & {
   name: string;
@@ -18,6 +19,7 @@ export type MProductPopulated = MProduct & {
   categories: MProductCategory[];
   variants: MVariant[];
   items: MProductItem[];
+  store: MStore;
 };
 
 export type MProductPreview = BaseModel & {
